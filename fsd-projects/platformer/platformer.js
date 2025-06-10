071c26ff3b35d94e84c38bdb043414d7754116e6
haskellcanman981 <haskell.lama@gmail.com>
2025-06-10 16:26:01 +0000

    finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,31 +27,35 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-     toggleGrid();
+     //toggleGrid();
 
 
     // TODO 2 - Create Platforms
-    createPlatform(900, 630, 150, 30, "green")
-    createPlatform(1100, 550, 150, 30, "green")
-    createPlatform(750, 470, 150, 30, "green")
-    createPlatform(500, 580, 15, 30, "green")
-    createPlatform(360, 500, 10, 30, "green")
-    createPlatform(100, 450, 100, 30, "green")
-    createPlatform(100, 320, 70, 30, "green")
-    createPlatform(300, 290, 10, 30, "green")
-    createPlatform(500, 290, 10, 30, "green")
-    createPlatform(700, 290, 10, 30, "green")
-    createPlatform(600, 290, 10, 30, "green")
-    createPlatform(400, 290, 10, 30, "green")
-    createPlatform(800, 250, 100, 30, "green")
-    createPlatform(1100, 300, 3, 30, "green")
-    createPlatform(1300, 200, 100, 30, "green")
-    createPlatform(1, 740, 2000, 30, "green")
+    createPlatform(900, 630, 150, 30, "#52c91a")
+    createPlatform(1100, 550, 150, 30, "#52c91a")
+    createPlatform(750, 470, 150, 30, "#52c91a")
+    createPlatform(500, 580, 15, 30, "#52c91a")
+    createPlatform(360, 500, 10, 30, "#52c91a")
+    createPlatform(100, 450, 100, 30, "#52c91a")
+    createPlatform(100, 320, 70, 30, "#52c91a")
+    createPlatform(100, 1, 3, 470, "#52c91a")
+    createPlatform(300, 290, 10, 30, "#52c91a")
+    createPlatform(500, 290, 10, 30, "#52c91a")
+    createPlatform(700, 290, 10, 30, "#52c91a")
+    createPlatform(600, 290, 10, 30, "#52c91a")
+    createPlatform(400, 290, 10, 30, "#52c91a")
+    createPlatform(800, 250, 100, 30, "#52c91a")
+    createPlatform(1100, 300, 3, 30, "#52c91a")
+    createPlatform(1300, 200, 100, 30, "#52c91a")
+    createPlatform(1, 740, 2000, 30, "#52c91a")
 
 
 
     // TODO 3 - Create Collectables
     createCollectable("ring", 1350, 150, 0.5, 0.7)
+    createCollectable("ring", 800, 400, 0.5, 0.7)
+    createCollectable("ring", 150, 400, 0.5, 0.7)
+    createCollectable("ring", 850, 200, 0.5, 0.7)
 
 
 
