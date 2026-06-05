2eec5ac9a2a93df770525dddf7c190d11b7d66a5
haskellcanman981 <haskell.lama@gmail.com>
2026-06-05 15:08:32 +0000

    finished dice project

--- asd-projects/dice-app/Dice.js
+++ asd-projects/dice-app/Dice.js
@@ -8,4 +8,50 @@ $("<div>")
 .css("top", 50)
 .css("left", 50)
 .appendTo("#die")
+function makeDot(top, left, elementID){
+$("<div>")
+.css("height", 15)
+.css("width", 15)
+.css ("background-color", "black")
+.css("position", "absolute")
+.css("top", top)
+.css("left", left)
+.appendTo(elementID)
+}
+function rollDie(dieID){
+$(dieID).empty();
+var randomNum = Math.ceil(Math.random() * 6);
+if (randomNum === 1) {
+makeDot(50, 50, dieID);
+} else if (randomNum == 2) {
+  makeDot(25, 25, dieID)
+  makeDot(75, 75, dieID)
+}  else if (randomNum === 3) {
+  makeDot(25, 25, dieID)
+  makeDot(75, 75, dieID)
+  makeDot(50, 50, dieID)
+} else if (randomNum === 4) {
+  makeDot(25, 25, dieID)
+  makeDot(75, 75, dieID)
+  makeDot(25, 75, dieID)
+  makeDot(75, 25, dieID)
+} else if (randomNum === 5) {
+  makeDot(50, 50, dieID)
+  makeDot(75, 75, dieID)
+  makeDot(25, 25, dieID)
+  makeDot(25, 75, dieID)
+  makeDot(75, 25, dieID)
+} else if (randomNum === 6) {
+  makeDot(75, 75, dieID)
+  makeDot(25, 25, dieID)
+  makeDot(25, 75, dieID)
+  makeDot(75, 25, dieID)
+  makeDot(50, 75, dieID)
+  makeDot(50, 25, dieID)
+}
+console.log(randomNum)
+}
+function handleClick(){
+rollDie("#die")
+} $("#die").on("click", handleClick);
 });
