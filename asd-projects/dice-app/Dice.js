0eb8f9d9c2cfd7337440ac411e01a870449e3ded
haskellcanman981 <haskell.lama@gmail.com>
2026-06-05 14:14:31 +0000

    unfinished Dice

---
+++ asd-projects/dice-app/Dice.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  // Your code goes here
+$("<div>")
+.css("height", 15)
+.css("width", 15)
+.css("background-color", "black")
+.css("position", "absolute")
+.css("top", 50)
+.css("left", 50)
+.appendTo("#die")
+});
